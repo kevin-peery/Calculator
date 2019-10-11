9fddccc4eba3b4b3c9dfd4ba04ef89db52c57b91
kevin-peery <kevin.peery@student.swic.edu>
2019-10-11 15:11:18 -0700

    Add method.

--- scripts.js
+++ scripts.js
@@ -1,48 +1,42 @@
 //A simple Calculator app.
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+//Declare methods
+const calculator = {
+  add(a, b) {
+    //Calculate
+    return Number(a) + Number(b);
+  },
 
-//Declare functions
-function adder(number1, number2) {
-  //Calculate
-  let result = Number(number1) + Number(number2);
+  subtract(a, b) {
+    //Calculate
+    return a - b;
+  },
 
-  //Display
-  console.log("Addition result: " + result);
-}
-function subtractor(number1, number2) {
-  //Calculate
-  let result = number1 - number2;
+  multiply(a, b) {
+    //Calculate
+    return a * b;
+  },
 
-  //Display
-  console.log("Subtraction result: " + result);
-}
-function multiplier(number1, number2) {
-  //Calculate
-  let result = number1 * number2;
+  divide(a, b) {
+    //Calculate
+    return a / b;
+  },
 
-  //Display
-  console.log("Multiplication result: " + result);
-}
-function divider(number1, number2) {
-  //Calculate
-  let result = number1 / number2;
+  remainder: function remainder(a, b) {
+    //Calculate
+    return a % b;
+  }
+};
 
-  //Display
-  console.log("Division result: " + result);
-}
-
+//Declare variables
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 form.addEventListener("submit", function(event) {
-  //
   event.preventDefault();
 
-  console.log(num1.value, num2.value, select.value);
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
